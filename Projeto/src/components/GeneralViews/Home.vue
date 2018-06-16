73371ea7abbc89d1683c76e394321055f4af6ef4
Vitor <vitorsellrs@gmail.com>
2018-06-16 16:42:42 -0300

    Adicionado a logo e resolvido alguns erros

---
+++ Projeto/src/components/GeneralViews/Home.vue
@@ -0,0 +1,20 @@
+<template>
+
+<div>
+<Header></Header>
+
+    <div>
+        texto qualquer
+    </div>
+
+</div>
+
+</template>
+
+<script> 
+import Header from '../Header/Header'
+
+    export default{
+        components: { Header }
+}
+</script>
---
+++ Projeto/src/components/Header/Header.vue
@@ -0,0 +1,15 @@
+<template> 
+<div>
+  <b-navbar variant="faded" type="light">
+    <b-navbar-brand href="#">
+      <img src="./src/assets/logo.png" class="d-inline-block align-top" alt="FINMAP">
+    </b-navbar-brand>
+  </b-navbar>
+</div>
+</template>
+
+<script> 
+    export default{
+
+}   
+</script>
