11c0eab2598f430c16a1781f0d01239e84d69b8f
Vitor <vitorsellrs@gmail.com>
2018-06-16 19:51:12 -0300

    adicionado botão de login

--- Projeto/src/components/Header/Header.vue
+++ Projeto/src/components/Header/Header.vue
@@ -1,12 +1,22 @@
 <template> 
     <div>
-        <b-navbar variant="danger" type="dark">
+        <b-navbar variant="dark" type="dark">
           <b-navbar-brand href="#">
             <img src="../../assets/logo2.png" class="d-inline-block align-top" alt="FINMAP" id="img">
             Financial Map
           </b-navbar-brand>
+
+          <b-navbar-brand href="#"> 
+            Como Funciona 
+          </b-navbar-brand>
+              
+          <b-nav-item-dropdown text="Login" id="li1" right>
+              <b-dropdown-item href="#">Fazer Login</b-dropdown-item>
+              <b-dropdown-item href="#">Cadastrar-se</b-dropdown-item>
+          </b-nav-item-dropdown>
+
         </b-navbar>
-      </div>
+    </div>
 </template>
 
 <script> 
@@ -19,4 +29,9 @@
   #img {
     max-width: 80px;
   }
+
+  #li1 {
+    list-style: none;
+  }
+
 </style>
