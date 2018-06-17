6e2e9e13e1c3cf15238ff00d172cd78c01cec94c
Vitor <vitorsellrs@gmail.com>
2018-06-17 17:16:07 -0300

    Criada área de cadastro

--- Projeto/src/components/GeneralViews/Cadastro.vue
+++ Projeto/src/components/GeneralViews/Cadastro.vue
@@ -8,22 +8,55 @@
     <b>CADASTRO</b>
 </div>
 
+<form id="form1">
+  <div class="form-group row">
+
+    <div class="col-sm-4">
+      Nome:
+      <input type="text" class="form-control" id="nome" placeholder="Digite seu nome..." required>
+      <small id="ajudaNome" class="form-text text-muted">
+        Ex: João
+      </small>
+    </div>
+
+    <div class="col-sm-4">
+      Sobrenome:
+      <input type="text" class="form-control" id="sobrenome" placeholder="Digite seu sobrenome..." required>
+      <small id="ajudaSobrenome" class="form-text text-muted">
+        Ex: Da Silva
+      </small>
+    </div>
+
+    <div class="col-sm-4">
+      Confirmar Senha
+      <input type="date" class="form-control" id="data">
+    </div>
+
+  </div>
+</form>
+
 <form id="form2">
   <div class="form-group row">
 
     <div class="col-sm-4">
       E-mail:
-      <input type="email" class="form-control" id="email" placeholder="Digite seu email...">
+      <input type="email" class="form-control" id="email" placeholder="Digite seu email..." required>
+      <small id="ajudaEmail" class="form-text text-muted">
+        Ex: joao@gmail.com
+      </small>
     </div>
 
     <div class="col-sm-4">
       Senha:
-      <input type="password" class="form-control" id="senha" placeholder="Digite sua senha...">
+      <input type="password" class="form-control" id="senha" placeholder="Digite sua senha..." required>
+      <small id="ajudaSenha" class="form-text text-muted">
+        Sua senha deve conter uma letra maíuscula no mínimo.
+      </small>
     </div>
 
     <div class="col-sm-4">
       Confirmar Senha
-      <input type="password" class="form-control" id="senha" placeholder="Digite sua senha novamente...">
+      <input type="password" class="form-control" id="senha" placeholder="Digite sua senha novamente..." required>
     </div>
 
   </div>
@@ -73,9 +106,10 @@ import Header from '../Header/Header'
     text-align: center;
 }
 
-#form2 {
+#form1, #form2 {
     border-left: 100px solid;
     border-right: 100px solid;
+    border-bottom: 15px solid;
     border-color: white;
 }
 
